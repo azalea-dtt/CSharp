63cdc8c3fe237328e0af95a182343725ef9f6e00
Thu-Thuy Do <79177932+azalea-dtt@users.noreply.github.com>
2021-11-11 22:54:56 +0900

    Add files via upload

---
+++ ServerProgram.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PipeComm
+{
+    class ServerProgram
+    {
+        static void Main(string[] args)
+        {
+            var server = new NamedPipeServerStream("PipesOfPiece");
+            Console.WriteLine("Server waiting...");
+            server.WaitForConnection();
+            StreamReader reader = new StreamReader(server);
+            StreamWriter writer = new StreamWriter(server);
+            while (true)
+            {
+                var line = reader.ReadLine();
+                Console.WriteLine(line);
+                writer.WriteLine(String.Join("", line.Reverse()));
+                writer.Flush();
+            }
+        }
+    }
+}
